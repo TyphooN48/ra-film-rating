3a20b33d3c07ff4462c88c234a106f311a28a5e5
TyphooN48 <andrew.burlackov@yandex.ru>
2024-01-04 15:26:14 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Stars from './components/Stars';
+import './App.css';
+
+function App() {
+  return (
+    <div className='panel'>
+      <Stars count={5} />
+    </div>
+  );
+}
+
+export default App;
